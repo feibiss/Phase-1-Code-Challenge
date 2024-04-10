551191bd92ae9eb2ab10b3c44d5894826816948c
feibiss <fabiansang@gmail.com>
2024-04-11 01:25:15 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log('hiddedn jobs')
+
+// // const students={
+// //     name :'wanjiru',
+// //     gender : 'female',
+// }
+// console.log(students.name); 
+
+const age = 10
+
+// if (age<20) {
+//     console.log("you can join moringa");
+// }else if(age >=18 && age <= 300){
+//     console.log("you cna go home");
+// }
+
+
