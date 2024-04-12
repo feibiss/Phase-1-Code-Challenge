e406212d48dcbe8a52c85e86a9d49a438ad432f2
feibiss <fabiansang@gmail.com>
2024-04-12 23:26:58 +0300

    added the json fetching

--- script.js
+++ script.js
@@ -1,33 +1,76 @@
-document.getElementById('fetchDataBtn').addEventListener('click', fetchData);
+const jobs = [
+ 
+];
 
-function fetchData() {
-  fetch('https://arbeitnow.com/api/job-board-api')
+
+const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+const jobSearch = document.querySelector(".jobs-list-container .job-search");
+
+let searchTerm = "";
+
+
+
+const createJobListingCards = async () => {
+  jobsContainer.innerHTML = "";
+
+  fetch('db.json')
     .then(response => response.json())
-    .then(data => {
-      displayData(data);
+    .then(d => {
+
+      const jobs = d.data;
+      if (jobs.length) {
+        filterAndDisplayJobs(jobs);
+      }
+
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+    .catch(error => console.error('Error:', error));
+
+};
+
+function filterAndDisplayJobs(jobs) {
+  jobs.forEach((job) => {
+    if (shouldDisplayJob(job)) {
+      createJobCard(job);
+    }
+  });
 }
 
-function displayData(data) {
-  const jobDataContainer = document.getElementById('jobData');
-  jobDataContainer.innerHTML = ''; // Clear previous data
-
-  if (data && data.data) {
-    data.data.forEach(job => {
-      const jobElement = document.createElement('div');
-      jobElement.classList.add('jobItem');
-      jobElement.innerHTML = `
-        <h2>${job.title}</h2>
-        <p>${job.company_name}</p>
-        <p>${job.description}</p>
-        <a href="${job.url}" target="_blank">View Job</a>
-      `;
-      jobDataContainer.appendChild(jobElement);
-    });
-  } else {
-    jobDataContainer.textContent = 'No jobs found';
-  }
+function shouldDisplayJob(job) {
+  return job.title.toLowerCase().includes(searchTerm.toLowerCase());
 }
+
+function createJobCard(job) {
+  let jobCard = document.createElement("div");
+  jobCard.classList.add("job");
+
+  let title = document.createElement("h1");
+  title.innerHTML = job.title;
+  title.classList.add("job-title");
+
+  let details = document.createElement("div");
+  details.innerHTML = job.description;
+  details.classList.add("details");
+
+  let detailsBtn = document.createElement("a");
+  detailsBtn.href = job.url;
+  detailsBtn.target = "_blank";
+  detailsBtn.innerHTML = "Apply";
+  detailsBtn.classList.add("details-btn");
+
+  jobCard.appendChild(title);
+  jobCard.appendChild(details);
+  jobCard.appendChild(detailsBtn);
+  jobsContainer.appendChild(jobCard);
+}
+
+(async function () {
+  await createJobListingCards();
+
+
+
+  jobSearch.addEventListener("input", (e) => {
+    searchTerm = e.target.value;
+
+    createJobListingCards();
+  });
+})()
